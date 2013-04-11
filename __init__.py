7e32ff3bafb02abb6835ea811ca603f577171398
Dan Allan <daniel.b.allan@gmail.com>
2013-04-11 14:01:07 -0400

    Reorganize namespaces

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from muxing import *
-
-try:
-    import cv2
-    from opencv import *
-except ImportError:
-    raise UserWarning, \
-    """The module cv2 could not be found. All dependent video tools
-    will not be imported."""
