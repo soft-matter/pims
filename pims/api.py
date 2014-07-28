a3939cd78282af42626c0c93f059381eca985207
Tom Dimiduk <tom@dimiduk.net>
2014-07-28 16:17:04 -0400

    prototype for a unified load function for all file types

--- pims/api.py
+++ pims/api.py
@@ -2,6 +2,9 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import six
+import glob
+import os
+from warnings import warn
 
 # has to be here for API stuff
 from pims.image_sequence import ImageSequence  # noqa
@@ -33,3 +36,55 @@ try:
         raise ImportError()
 except ImportError:
     TiffStack = not_available("libtiff or PIL/PILLOW")
+
+def open(sequence, process_func=None, dtype=None, as_gray=False, plugin=None):
+    """Read a directory of sequentially numbered image files into an
+    iterable that returns images as numpy arrays.
+
+    Parameters
+    ----------
+    sequence : string, list of strings, or glob
+       The sequence you want to load. This can be a directory containing images,
+       a glob of images, a video file, or a tiff stack
+    process_func : function, optional
+        callable with signalture `proc_img = process_func(img)`,
+        which will be applied to the data from each frame
+    dtype : numpy datatype, optional
+        Image arrays will be converted to this datatype.
+    as_grey : boolean, optional
+        Convert color images to greyscale. False by default.
+        May not be used in conjection with process_func.
+    plugin : string
+        Passed on to skimage.io.imread if scikit-image is available.
+        If scikit-image is not available, this will be ignored and a warning
+        will be issued.
+
+    Examples
+    --------
+    >>> video = open('path/to/images/*.png')  # or *.tif, or *.jpg
+    >>> imshow(video[0]) # Show the first frame.
+    >>> imshow(video[-1]) # Show the last frame.
+    >>> imshow(video[1][0:10, 0:10]) # Show one corner of the second frame.
+
+    >>> for frame in video[:]:
+    ...    # Do something with every frame.
+
+    >>> for frame in video[10:20]:
+    ...    # Do something with frames 10-20.
+
+    >>> for frame in video[[5, 7, 13]]:
+    ...    # Do something with frames 5, 7, and 13.
+
+    >>> frame_count = len(video) # Number of frames in video
+    >>> frame_shape = video.frame_shape # Pixel dimensions of video
+    """
+    files = glob.glob(sequence)
+    if len(sequence) > 1:
+        return ImageSequence(files, process_func, dtype, as_gray, plugin)
+    if plugin is not None:
+        warn("scikit-image plugin specification ignored because such plugins"
+             "only apply when loading a sequence of image files. ")
+    tok = os.path.splitext(sequence)
+    if tok[1].lower() in ['.tif', '.tiff']:
+        return TiffStack(sequence, process_func, dtype, as_gray)
+    return Video(sequence, process_func, dtype, as_array)
