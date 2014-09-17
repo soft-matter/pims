9534d5082494a6dd8fda17a8de68f8a5010a303e
danielballan <daniel.b.allan@gmail.com>
2014-09-17 19:13:38 -0400

    Use versioneer.

--- pims/__init__.py
+++ pims/__init__.py
@@ -1,3 +1,4 @@
 from pims.api import *
-
-from pims.version import version as __version__
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -24,62 +24,16 @@ except ImportError:
     from numpy.distutils.core import setup
     _have_setuptools = False
 
-MAJOR = 0
-MINOR = 2
-MICRO = 0
-ISRELEASED = True
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-QUALIFIER = ''
-
-FULLVERSION = VERSION
-print(FULLVERSION)
-
-if not ISRELEASED:
-    FULLVERSION += '.dev'
-    try:
-        import subprocess
-        try:
-            pipe = subprocess.Popen(["git", "describe", "HEAD"],
-                                    stdout=subprocess.PIPE).stdout
-        except OSError:
-            # msysgit compatibility
-            pipe = subprocess.Popen(
-                ["git.cmd", "describe", "HEAD"],
-                stdout=subprocess.PIPE).stdout
-        rev = pipe.read().strip()
-        # makes distutils blow up on Python 2.7
-        import sys
-        if sys.version_info[0] >= 3:
-            rev = rev.decode('ascii')
-
-        FULLVERSION = rev.lstrip('v')
-
-    except:
-        warnings.warn("WARNING: Couldn't get git revision")
-else:
-    FULLVERSION += QUALIFIER
-
-
-def write_version_py(filename=None):
-    cnt = """\
-version = '%s'
-short_version = '%s'
-"""
-    if not filename:
-        filename = os.path.join(
-            os.path.dirname(__file__), 'pims', 'version.py')
-
-    a = open(filename, 'w')
-    try:
-        a.write(cnt % (FULLVERSION, VERSION))
-    finally:
-        a.close()
-
-write_version_py()
-
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'pims/_version.py'
+versioneer.versionfile_build = 'pims/_version.py'
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = '.'
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup_parameters = dict(
     name="PIMS",
