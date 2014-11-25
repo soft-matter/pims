d8a35f0b0380338b7685daff753b8adb16531f9d
caspervdw <caspervdw@gmail.com>
2014-11-25 17:32:20 +0100

    ENH: Multidimensional image-sequence importer
    - imports t, z, c indexed tif files. output is indexed with t index.

--- pims/image_sequence.py
+++ pims/image_sequence.py
@@ -8,6 +8,7 @@ import glob
 from warnings import warn
 from pims.base_frames import FramesSequence
 from pims.frame import Frame
+import numpy as np
 
 # skimage.io.plugin_order() gives a nice hierarchy of implementations of imread.
 # If skimage is not available, go down our own hard-coded hierarchy.
@@ -18,8 +19,7 @@ except ImportError:
         from matplotlib.pyplot import imread
     except ImportError:
         from scipy.ndimage import imread
-
-
+    
 class ImageSequence(FramesSequence):
     """Read a directory of sequentially numbered image files into an
     iterable that returns images as numpy arrays.
@@ -153,3 +153,114 @@ Pixel Datatype: {dtype}""".format(w=self.frame_shape[0],
                                   count=len(self),
                                   pathname=source,
                                   dtype=self.pixel_type)
+ 
+def tzcfromfilename(filename):
+    def toint(s):
+        result = str(s)
+        while result[0] == "0":
+            result = result[1:]
+        return int(result)
+    filename_rev = filename[::-1]
+    extension = filename_rev.find('.')
+    filename_rev = filename_rev[extension + 1:]
+    dimensions = np.array(['t', 'z', 'c'])
+    positions = np.array([len(filename_rev) - filename_rev.find(s) for s in dimensions])
+    order = positions.argsort()
+    dimensions = dimensions[order]
+    positions = positions[order]
+    d0 = toint(filename[positions[0]:positions[1] - 1])
+    try:
+        d0 = toint(filename[positions[0]:positions[1] - 1])
+    except:
+        d0 = 0
+    try:
+        d1 = toint(filename[positions[1]:positions[2] - 1])
+    except:
+        d1 = 0
+    try:
+        d2 = toint(filename[positions[2]:len(filename) - extension - 1])
+    except:
+        d2 = 0
+    result = {dimensions[0]: d0, dimensions[1]: d1, dimensions[2]: d2}
+    return (result['t'], result['z'], result['c'])
+                                
+                                
+class ImageSequence3D(ImageSequence):
+    def _get_files(self, path_spec):
+        super(ImageSequence3D, self)._get_files(path_spec) 
+        self._toc = np.array([tzcfromfilename(f) for f in self._filepaths])
+        for n in range(3):
+            if min(self._toc[:,n]) == 1:
+                self._toc[:,n] = self._toc[:,n] - 1            
+        self._filepaths = np.array(self._filepaths)
+        self._count = max(self._toc[:,0])
+        self._sizeZ = max(self._toc[:,1]) + 1
+        self._sizeC = max(self._toc[:,2]) + 1
+        self._pixelX = None
+        self._pixelY = None
+        self._pixelZ = None
+        self._channel = list(range(0, self._sizeC))
+        
+    def get_frame(self, j):
+        if j > self._count:
+            raise ValueError("File does not contain this many frames")
+            
+        res = np.empty((len(self._channel), self._sizeZ, 
+                        self._first_frame_shape[0], self._first_frame_shape[1]))        
+
+        for (Nc, c) in enumerate(self._channel):
+            selector = np.logical_and(self._toc[:,0] == j, self._toc[:,2] == c)
+            filelist = self._filepaths[selector]
+            for (z, loc) in enumerate(filelist):
+                res[Nc, z] = imread(loc, **self.kwargs)
+         
+        if res.dtype != self._dtype:
+            res = res.astype(self._dtype)
+        
+        return Frame(self.process_func(res.squeeze()), frame_no=j)
+    
+    @property   
+    def sizes(self):
+        return {'X': self._first_frame_shape[1], 
+                'Y': self._first_frame_shape[0],
+                'Z': self._sizeZ,
+                'T': self._count,
+                'C': self._sizeC}
+                
+    @property   
+    def pixelsizes(self):
+        return {'X': self._pixelX, 
+                'Y': self._pixelY,
+                'Z': self._pixelZ}
+                
+    @property
+    def channel(self):
+        return self._channel
+    @channel.setter
+    def channel(self, value):
+        if not hasattr(value, '__iter__'):
+            value = [value]
+        if np.any(np.greater_equal(value, self._sizeC)) or np.any(np.less(value, 0)):
+            raise IndexError('Channel index out of bounds.')
+        self._channel = value
+            
+                
+    def __repr__(self):
+        # May be overwritten by subclasses
+        try:
+            source = self.pathname
+        except AttributeError:
+            source = '(list of images)'
+        return """<Frames>
+Source: {pathname}
+SizeT: {count} frames
+SizeZ: {Z} frames
+SizeC: {C} frames
+Frame Shape: {w} x {h}
+Pixel Datatype: {dtype}""".format(w=self.frame_shape[0],
+                                  h=self.frame_shape[1],
+                                  count=len(self),
+                                  pathname=source,
+                                  dtype=self.pixel_type,
+                                  C = self._sizeC,
+                                  Z = self._sizeZ)
