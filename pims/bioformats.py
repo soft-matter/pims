b668b8b74950f2ec696583db354ad2370454292b
caspervdw <caspervdw@gmail.com>
2014-11-26 16:49:56 +0100

    Docstrings, minor changes, rescale=False
    - put `rescale = False` in bioformats `reader.read`, now really the raw
    data is returned and there is no conversion
    - added docstrings in numpydoc format
    - C is now processed internally as tuple
    - metadata in `BioformatsReader3D` is now complete with a record for
    every indexC and every indexZ

--- pims/bioformats.py
+++ pims/bioformats.py
@@ -63,48 +63,80 @@ def jwtoauto(jwrapper):
 class BioformatsReader2D(FramesSequence):
     """Reads 2D images from the frames of a file supported by bioformats into an
     iterable object that returns images as numpy arrays.
+
+    Parameters
+    ----------
+    filename: str
+    series: int, optional
+        Active image series index, defaults to 0. Changeable via the `series` property.
+    process_func: function, optional
+        callable with signature `proc_img = process_func(img)`,
+        which will be applied to the data from each frame
+    dtype: numpy.dtype, optional
+        unused
+    as_grey: bool, optional
+        unused        
     
-    Required:
+    Attributes
+    ----------
+    __len__ : int
+        Number of planes in active series (= size Z*C*T)
+    sizes : dict of int
+        Number of series and for active series: X, Y, Z, C, T sizes
+    frame_shape : tuple of int
+        Sizes in pixels in X, Y. Equal to (sizes['X'], sizes['Y'])
+    pixelsizes : dict of float
+        Physical pixelsizes in X, Y, Z (in microns)
+    series : int
+        active series that is read by get_frame. Writeable.
+    channel : int or list of int
+        channel(s) that are read by get_frame. Writeable.
+    omexml : bioformats.OMEXML object
+        returns the bioformats.OMEXML object. Very slow for large files.
+        see https://github.com/CellProfiler/python-bioformats
+        
+    Methods
+    ----------
+    get_frame(plane) : pims.frame object
+        returns 2D image in active series. See notes for metadata content.
+    metadataretrieve(mdr, *args) : float or int or str
+        returns the result of loci.formats.meta.MetadataRetrieve.<mdr>(*args)
+        documentation on MetadataRetrieve methods can be found here:
+        http://downloads.openmicroscopy.org/bio-formats/5.0.4/api/loci/formats/meta/MetadataRetrieve.html
+
+    
+    Examples
+    ----------
+    >>> frames.metadataretrieve('getPlaneDeltaT', 0, 50)
+    ...    # evaluates MetadataRetrieve.getPlaneDeltaT(0, 50)
+    
+    Notes
+    ----------
+    Dependencies:
     https://github.com/CellProfiler/python-bioformats
     https://github.com/CellProfiler/python-javabridge
      or (windows compiled) http://www.lfd.uci.edu/~gohlke/pythonlibs/#javabridge   
     
     Only tested with Nikon ND2 files
-    For files larger than 4GB, 64 bits Python, Javabridge and JDK are required
-
-    Parameters
-    ----------
-    filename: string
-    process_func: function, optional
-        callable with signature `proc_img = process_func(img)`,
-        which will be applied to the data from each frame
-    series: active image series index (change with property series)
+    For files larger than 4GB, 64 bits Python is required
+    Does not support RGB files
     
-    Metadata
-    ----------
-    Standard metadata: 
-     - number of series
-     - per series:   - size C, T, Z, Y, X
-                     - physical pixelsizes X, Y, Z
-     - per frame:         - indices plane, series, Z, C, T
-        (as dictionary)   - physical locations X, Y, Z, T
-                    
-     - more can be read out using metadataretrieve, passing the method as a 
-     string. All further arguments will be passed to that method. Example: 
-     
-         frames.metadataretrieve('getPlaneDeltaT', 0, 50)
-         evaluates MetadataRetrieve.getPlaneDeltaT(0, 50)
-     
-     documentation on MetadataRetrieve methods can be found here:
-     http://downloads.openmicroscopy.org/bio-formats/5.0.4/api/loci/formats/meta/MetadataRetrieve.html
-     - a bioformats.OMEXML object can also be obtained using .omexml, see 
-     https://github.com/CellProfiler/python-bioformats"""
-     
+    Metadata provided by get_frame, as dictionary: 
+        plane: index of image in series
+        series: series index
+        indexC, indexZ, indexT: indexes of C, Z, T 
+        X, Y, Z: physical location of the image in microns
+        T: timestamp of the image in seconds
+    """
+    
     @classmethod
     def class_exts(cls):
         return {'nd2'} | super(BioformatsReader2D, cls).class_exts()
 
-    def __init__(self, filename, series=0, process_func=None, as_grey=False):
+    def __init__(self, filename, series=0, process_func=None, dtype=None, 
+                 as_grey=False):
+        if dtype != None:
+            raise NotImplementedError('This reader does not support dtype casting')
         self.filename = str(filename)
         self._series = series        
         self._validate_process_func(process_func)
@@ -112,6 +144,8 @@ class BioformatsReader2D(FramesSequence):
         self._change_series() 
     
     def _initializereader(self):
+        """Starts java VM, creates reader and MetadataStore
+        """
         javabridge.start_vm(class_path=bioformats.JARS,max_heap_size='512m')
         self._reader = bioformats.get_image_reader(self.filename, self.filename) 
         if self._reader.rdr.isRGB():
@@ -121,7 +155,10 @@ class BioformatsReader2D(FramesSequence):
         self._metadatacolumns = ['plane', 'series', 'indexC', 'indexZ', 
                                  'indexT','X', 'Y', 'Z', 'T']
            
-    def _change_series(self):   
+    def _change_series(self):  
+        """Changes series and rereads dtype, sizes and pixelsizes. 
+        When pixelsize Y is not found, pixels are assumed to be square.
+        """
         series = self._series   
         self._reader.rdr.setSeries(series)
         
@@ -147,7 +184,6 @@ class BioformatsReader2D(FramesSequence):
     def close(self):  
         bioformats.release_image_reader(self.filename)
         
-
     @property
     def pixelsizes(self):
         return {'X': self._pixelX, 'Y': self._pixelY, 'Z': self._pixelZ}        
@@ -174,17 +210,24 @@ class BioformatsReader2D(FramesSequence):
         return self._sizeX, self._sizeY
         
     def get_frame(self, j):
+        """Wrapper for _get_frame, additionally applies the process_func and
+        converts the numpy array and metadata to a Frame object.
+        """
         im, metadata = self._get_frame(self.series, j)
         imageproc = self.process_func(im)
         return Frame(imageproc, frame_no=j, metadata=metadata) 
         
     def _get_frame(self, series, j):
+        """Returns image as 2D numpy array and metadata as dictionary.
+        """
         im, metadata = self._get_frame_2D(series, j)
         metadataproc = dict(zip(self._metadatacolumns, metadata))        
         return im, metadataproc
         
-    def _get_frame_2D(self, series, j):       
-        im = self._reader.read(series=series, index=j)    
+    def _get_frame_2D(self, series, j): 
+        """Actual reader, returns image as 2D numpy array and metadata as tuple.
+        """
+        im = self._reader.read(series=series, index=j, rescale = False)    
 
         metadata = (j,
                 series,
@@ -214,7 +257,6 @@ class BioformatsReader2D(FramesSequence):
         return self._pixel_type
 
     def __repr__(self):
-        # May be overwritten by subclasses
         result = """<Frames>
             Source: {filename}
             Series: {mp}, active: {mpa}
@@ -235,34 +277,46 @@ class BioformatsReader2D(FramesSequence):
         
         
 class BioformatsReader3D(BioformatsReader2D):
-    """Extends BioformatsReader2D
-    Reads 3D images from the frames of a file supported by bioformats into an
+    """Reads 3D images from the frames of a file supported by bioformats into an
     iterable object that returns images as numpy arrays, indexed by T index.
 
     Parameters
     ----------
-    filename : string
-    process_func : function, optional
-        callable with signature `proc_img = process_func(img)`,
-        which will be applied to the data from each frame
-    C : list of color channels to read (change with property channel)
-    series : active Series index (change with property series)
+    C : int or list of int
+        Channel(s) that are read by get_frame. Changeable via the `channel` property.
     
-    Metadata
+    Attributes
     ----------
-    see BioformatsReader2D class docstring
-    
+    __len__ : int
+        Number of timepoints in active series (equal to sizes['T'])
+    channel : int or iterable of int
+        channel(s) that are read by get_frame. Writeable.
+        
+    Methods
+    ----------
+    get_frame(t) : pims.frame object
+        returns 3D image in active series. See notes for metadata content.
+        
+    Notes
+    ----------    
+    Metadata provided by get_frame, as DataFrame with following columns: 
+        plane: index of image in series
+        series: series index      
+        indexC, indexZ, indexT: indexes of C, Z and T
+        X, Y, Z: physical location of the image in microns
+        T: timestamp of the image in seconds
     """
     @classmethod
     def class_exts(cls):
         return {'nd2'} | super(BioformatsReader3D, cls).class_exts()
 
-    def __init__(self, filename, mode='auto', C=[0], series=0, 
-                 process_func=None, as_grey=False):
-        if not hasattr(C, '__iter__'):
-            C = [C]    
-        self._channel = C
-        
+    def __init__(self, filename, C=(0,), series=0, 
+                 process_func=None, dtype=None, as_grey=False):
+        try:
+            self._channel = tuple(C)
+        except TypeError:
+            self._channel = tuple((C,))
+            
         super(BioformatsReader3D, self).__init__(filename, series, process_func)                
         
     def __len__(self):
@@ -273,26 +327,29 @@ class BioformatsReader3D(BioformatsReader2D):
         return self._channel
     @channel.setter
     def channel(self, value):
-        if not hasattr(value, '__iter__'):
-            value = [value]
-        if np.any(np.greater_equal(value, self._sizeC)) or np.any(np.less(value, 0)):
+        try:
+            channel = tuple(value)
+        except TypeError:
+            channel = tuple((value,))
+        if np.any(np.greater_equal(channel, self._sizeC)) or np.any(np.less(channel, 0)):
             raise IndexError('Channel index out of bounds.')
-        if value != self._channel:
-            self._channel = value
+        self._channel = channel
                 
     def _get_frame(self, series, t):
-        imlist = np.empty((len(self.channel), self._sizeZ, 
-                           self._sizeY, self._sizeX))
-        metadata = np.empty((self._sizeZ, len(self._metadatacolumns)))  
+        """Builds array of images and DataFrame of metadata.
+        """
+        imlist = np.zeros((len(self.channel), self._sizeZ, 
+                           self._sizeY, self._sizeX), dtype=self.pixel_type)
+        metadata = []
                            
         for (Nc, c) in enumerate(self.channel):            
             for z in range(self._sizeZ):
-                imlist[Nc, z], metadata[z] = \
-                    self._get_frame_2D(series, self._reader.rdr.getIndex(z,c,t))    
+                imlist[Nc, z], md = self._get_frame_2D(series, 
+                                          self._reader.rdr.getIndex(z,c,t))
+                metadata.append(md)
 
         if DataFrame != None:
             metadata = DataFrame(metadata, columns=self._metadatacolumns)
-            if len(self.channel) > 1:
-                metadata = metadata.drop('indexC', 1)
+            metadata.set_index(['indexC','indexZ'], drop=False, inplace=True)
         
         return imlist.squeeze(), metadata
