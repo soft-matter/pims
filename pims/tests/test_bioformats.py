163f59786279fee3de64f973604899f95e824d29
caspervdw <caspervdw@gmail.com>
2015-01-27 14:14:35 +0100

    Added metadata tests
    - imports _image_single and _image_series from `test_common`
    - greyscale tests fail on 3D and multichannel images, this is solved by
    PR #131

--- pims/tests/test_bioformats.py
+++ pims/tests/test_bioformats.py
@@ -7,9 +7,10 @@ import os
 import unittest
 import nose
 import numpy as np
-from numpy.testing import (assert_equal, assert_allclose)
+from numpy.testing import (assert_equal, assert_almost_equal, assert_allclose)
 
 import pims
+from test_common import _image_single, _image_series
 
 try:
     import javabridge
@@ -36,105 +37,11 @@ def assert_image_equal(actual, expected):
         assert_allclose(actual, expected, atol=1/256.)
 
 
-class _image_single(unittest.TestCase):
+class _image_stack(unittest.TestCase):
     def check_skip(self):
         pass
 
-    def test_getting_single_frame(self):
-        self.check_skip()
-        assert_image_equal(self.v[0], self.frame0)
-
-    def test_shape(self):
-        self.check_skip()
-        assert_equal(self.v.frame_shape, self.expected_shape)
-
-    def test_count(self):
-        self.check_skip()
-        assert_equal(len(self.v), self.expected_len)
-
-    def test_bool(self):
-        self.check_skip()
-        pass
-
-    def test_repr(self):
-        self.check_skip()
-        # simple smoke test, values not checked
-        repr(self.v)
-        
-    def test_dtype_conversion(self):
-        self.check_skip()
-        v8 = self.klass(self.filename, dtype='uint8', **self.kwargs)
-        v16 = self.klass(self.filename, dtype='uint16', **self.kwargs)
-        type8 = v8[0].dtype
-        type16 = v16[0].dtype
-        self.assertEqual(type8, np.uint8)
-        self.assertEqual(type16, np.uint16)
-
-    def test_process_func(self):
-        self.check_skip()
-        # Use a trivial identity function to verify the process_func exists.
-        f = lambda x: x
-        self.klass(self.filename, process_func=f, **self.kwargs)
-
-        # Also, it should be the second positional arg for each class.
-        # This is verified more directly in later tests, too.
-        self.klass(self.filename, f, **self.kwargs)
-
-
-class _image_series(_image_single):
-    def check_skip(self):
-        pass
-
-    def test_getting_slice(self):
-        self.check_skip()
-        tmp = list(self.v[0:2])
-        frame0, frame1 = tmp
-        assert_image_equal(frame0, self.frame0)
-        assert_image_equal(frame1, self.frame1)
-
-    def test_getting_list(self):
-        self.check_skip()
-        actual = list(self.v[[1, 0, 0, 1, 1]])
-        expected = [self.frame1, self.frame0, self.frame0, self.frame1,
-                    self.frame1]
-        [assert_image_equal(a, b) for a, b in zip(actual, expected)]
-
-    def test_integer_attributes(self):
-        self.check_skip()
-        assert_equal(len(self.v.frame_shape), len(self.expected_shape))
-        self.assertTrue(isinstance(self.v.frame_shape[0], int))
-        self.assertTrue(isinstance(self.v.frame_shape[1], int))
-        self.assertTrue(isinstance(len(self.v), int))
-
-    def test_simple_negative_index(self):
-        self.check_skip()
-        self.v[-1]
-        list(self.v[[0, -1]])
-
-    def test_frame_number_present(self):
-        self.check_skip()
-        for frame_no in [0, 1, 2, 1]:
-            self.assertTrue(hasattr(self.v[frame_no], 'frame_no'))
-            not_none = self.v[frame_no].frame_no is not None
-            self.assertTrue(not_none)
-
-    def test_frame_number_accurate(self):
-        self.check_skip()
-        for frame_no in [0, 1, 2, 1]:
-            self.assertEqual(self.v[frame_no].frame_no, frame_no)
-
-    def test_iterator(self):
-        self.check_skip()
-        i = iter(self.v)
-        assert_image_equal(next(i), self.frame0)
-        assert_image_equal(next(i), self.frame1)
-
-
-class _image_stack(_image_single):
-    def check_skip(self):
-        pass
-
-    def test_getting_single_frame(self):
+    def test_getting_stack(self):
         self.check_skip()
         assert_image_equal(self.v[0][0], self.frame0)
         assert_image_equal(self.v[0][1], self.frame1)
@@ -144,9 +51,27 @@ class _image_stack(_image_single):
         assert_equal(self.v.sizes['Z'], self.expected_Z)
 
 
-class TestND2(_image_series):
+class _image_multichannel(unittest.TestCase):
+    def check_skip(self):
+        pass
+
+    def test_change_channel(self):
+        self.check_skip()
+        self.v.channel = (0, 1)
+        channel0, channel1 = self.v[0][0], self.v[0][1]
+        self.v.channel = 0
+        assert_image_equal(self.v[0], channel0)
+        self.v.channel = 1
+        assert_image_equal(self.v[0], channel1)
+
+    def test_sizeC(self):
+        self.check_skip()
+        assert_equal(self.v.sizes['C'], self.expected_C)
+
+
+class TestND2(_image_series, _image_multichannel):
     # Nikon NIS-Elements ND2
-    # 38 x 31 pixels, 16 bits per sample, 3 time points, 10 focal planes
+    # 38 x 31 pixels, 16 bits, 2 channels, 3 time points, 10 focal planes
     def check_skip(self):
         _skip_if_no_bioformats()
 
@@ -160,13 +85,15 @@ class TestND2(_image_series):
         self.v = self.klass(self.filename, **self.kwargs)
         self.expected_shape = (31, 38)
         self.expected_len = 3
+        self.expected_Z = 10
+        self.expected_C = 2
 
     def tearDown(self):
         self.v.close()
 
 
 class TestIPL(_image_single):
-    # IPLab format, 650 x 515 pixels, 8 bits per sample, 3 channels
+    # IPLab format, 650 x 515 pixels, 8 bits per sample, RGB
     # Scanalytics has provided a sample multi-channel image in IPLab format.
     def check_skip(self):
         _skip_if_no_bioformats()
@@ -185,7 +112,7 @@ class TestIPL(_image_single):
         self.v.close()
 
 
-class TestSEQ(_image_stack):
+class TestSEQ(_image_single, _image_stack):
     # Image-Pro sequence format
     # 512 x 512 pixels, 8 bits per sample, 30 focal planes
     def check_skip(self):
@@ -207,7 +134,7 @@ class TestSEQ(_image_stack):
         self.v.close()
 
 
-class TestLEI(_image_stack):
+class TestLEI(_image_single, _image_stack):
     # Leica format
     # 256 x 256 pixels, 8 bits per sample, 3 focal planes
     # Clay Glennon of the Wisconsin National Primate Research Center at the
@@ -254,7 +181,51 @@ class TestICS(_image_single):
         self.v.close()
 
 
-# TODO: Metadata tests
+class TestMetadata(unittest.TestCase):
+    def check_skip(self):
+        _skip_if_no_bioformats()
+
+    def setUp(self):
+        _skip_if_no_bioformats()
+        self.filename = os.path.join(path, 'cluster.nd2')
+        self.klass = pims.Bioformats
+
+    def tearDown(self):
+        self.v.close()
+
+    def test_metadataretrieve(self):
+        # tests using the metadata object are combined in one, to reduce the
+        # amount of log output.
+        self.v = self.klass(self.filename, meta=True, C=0)
+        # test fields directly
+        assert_equal(self.v.metadata.getChannelCount(0), 2)
+        assert_equal(self.v.metadata.getChannelName(0, 0), '5-FAM/pH 9.0')
+        assert_almost_equal(self.v.metadata.getPixelsPhysicalSizeX(0),
+                            0.167808983)
+        # test metadata in Frame objects
+        assert_almost_equal(self.v[0].metadata['T'], 0.445083498)
+        assert_equal(self.v[0].metadata['indexT'], 0)
+        # test changing frame_metadata
+        del self.v.frame_metadata['T']
+        assert 'T' not in self.v[0].metadata
+        self.v.frame_metadata['T'] = 'getPlaneDeltaT'
+        assert 'T' in self.v[0].metadata
+        # test colors field
+        assert_allclose(self.v[0].metadata['colors'][0], [0.47, 0.91, 0.06],
+                        atol=0.01)
+
+    def test_metadata_raw(self):
+        self.v = self.klass(self.filename, meta=False, C=0)
+        metadata = self.v.get_metadata_raw('dict')
+        assert_equal(metadata['ChannelCount'], '2')
+        assert_equal(metadata['CH2ChannelDyeName'], '5-FAM/pH 9.0')
+        assert_equal(metadata['dCalibration'], '0.16780898323268245')
+
+    def test_metadata_omexml(self):
+        self.v = self.klass(self.filename, meta=False, C=0)
+        omexml = self.v.get_metadata_omexml()
+        assert_equal(omexml.image().Pixels.SizeC, 2)
+        assert_equal(omexml.image().Pixels.Channel(0).Name, '5-FAM/pH 9.0')
 
 
 class zzzKillVM(unittest.TestCase):
