79cf8e337ce32c044a8da688f48db364d0fc7c5f
caspervdw <caspervdw@gmail.com>
2015-11-08 20:08:00 +0100

    ENH Imageio reader
    Add __iter__ and correct metadata propagation

--- pims/imageio_reader.py
+++ pims/imageio_reader.py
@@ -15,24 +15,34 @@ except ImportError:
 def available():
     return imageio is not None
 
+
 class ImageIOReader(FramesSequence):
-    def __init__(self, filename):
+    def __init__(self, filename, **kwargs):
         if imageio is None:
             raise ImportError('The ImageIOReader requires imageio to work.')
-        self.reader = imageio.get_reader(filename)
+        self.reader = imageio.get_reader(filename, **kwargs)
         self.filename = filename
-        self._len = len(self.reader)
+        self._len = self.reader.get_length()
 
         first_frame = self.get_frame(0)
         self._shape = first_frame.shape
         self._dtype = first_frame.dtype
 
     def get_frame(self, i):
-        return Frame(self.reader.get_data(i), frame_no=i,
-                     metadata=self.reader.get_meta_data(i))
+        frame = self.reader.get_data(i)
+        return Frame(frame, frame_no=i, metadata=frame.meta)
+
+    def get_metadata(self):
+        return self.reader.get_meta_data(None)
 
     def __len__(self):
         return self._len
+
+    def __iter__(self):
+        iterable = self.reader.iter_data()
+        for i in range(len(self)):
+            frame = next(iterable)
+            yield Frame(frame, frame_no=i, metadata=frame.meta)
 
     @property
     def frame_shape(self):
@@ -40,7 +50,7 @@ class ImageIOReader(FramesSequence):
 
     @property
     def pixel_type(self):
-        return self._dtype  
+        return self._dtype
 
     def close(self):
         self.reader.close()
