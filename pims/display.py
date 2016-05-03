2946a2168e6c0d0e3545c83433039f86a3592759
caspervdw <caspervdw@gmail.com>
2016-05-03 12:17:17 +0200

    Limit export rate, add wmv2 quality

--- pims/display.py
+++ pims/display.py
@@ -31,7 +31,6 @@ try:
 except ImportError:
     VideoClip = None
 
-
 def export_pyav(sequence, filename, rate=30, bitrate=None,
                 width=None, height=None, codec='mpeg4', format='yuv420p',
                 autoscale=True):
@@ -139,7 +138,7 @@ def play(sequence, rate=30, bitrate=None,
 def export_moviepy(sequence, filename, rate=30, bitrate=None, width=None,
                    height=None, codec='libx264', format='yuv420p',
                    autoscale=True, quality=None, verbose=True,
-                   ffmpeg_params=None):
+                   ffmpeg_params=None, rate_range=(16, 32)):
     """Export a sequence of images as a standard video file.
 
     N.B. If the quality and detail are insufficient, increase the
@@ -166,9 +165,10 @@ def export_moviepy(sequence, filename, rate=30, bitrate=None, width=None,
         a valid video encoding, 'mpeg4' by default
     format: string, optional
         Video stream format, 'yuv420p' by default.
-    quality: integer or string, optional
+    quality: number or string, optional
         For 'mpeg4' codec: sets qscale:v. 1 = high quality, 5 = default.
         For 'libx264' codec: sets crf. 0 = lossless, 23 = default.
+        For 'wmv2' codec: sets fraction of lossless bitrate, 0.01 = default
     autoscale : boolean, optional
         Linearly rescale the brightness to use the full gamut of black to
         white values. True by default.
@@ -187,29 +187,39 @@ def export_moviepy(sequence, filename, rate=30, bitrate=None, width=None,
 
     if ffmpeg_params is None:
         ffmpeg_params = dict()
+    export_rate = _normalize_framerate(rate, *rate_range)
+
+    if codec == 'wmv2' and bitrate is None and quality is None:
+        quality = 0.01
+
     if quality is not None:
         if codec == 'mpeg4':
             ffmpeg_params['qscale:v'] = str(quality)
         elif codec == 'libx264':
             ffmpeg_params['crf'] = str(quality)
+        elif codec == 'wmv2':
+            if bitrate is not None:
+                warnings.warn("(wmv) quality is ignored when bitrate is set.")
+            else:
+                bitrate = quality * _estimate_bitrate(sequence.frame_shape,
+                                                      export_rate)
     if format is not None:
         ffmpeg_params['pixel_format'] = str(format)
-    if bitrate is None:
-        bitrate = _estimate_bitrate(sequence[0].shape, rate)
+    if bitrate is not None:
+        bitrate = str(bitrate)
+    if rate <= 0:
+        raise ValueError
 
     _ffmpeg_params = []
     [_ffmpeg_params.extend(['-' + key, ffmpeg_params[key]])
      for key in ffmpeg_params]
 
-    if rate < 10:
-        warnings.warn('Framerates lower than 10 may give playback issues.')
-
-    clip = VideoClip(lambda t: _to_rgb_uint8(sequence[int(round(t*rate))],
+    clip = VideoClip(lambda t: _to_rgb_uint8(sequence[int(t * rate)],
                                              autoscale))
     clip.duration = (len(sequence) - 1) / rate
     if not (height is None and width is None):
         clip = clip.resize(height=height, width=width)
-    clip.write_videofile(filename, rate, codec, str(bitrate), audio=False,
+    clip.write_videofile(filename, export_rate, codec, bitrate, audio=False,
                          verbose=verbose, ffmpeg_params=_ffmpeg_params)
 
 if av is not None:
@@ -386,6 +396,24 @@ def _estimate_bitrate(shape, frame_rate):
     "Return a bitrate that will guarantee lossless video."
     # Total Pixels x 8 bits x 3 channels x FPS
     return shape[0] * shape[1] * 8 * 3 * frame_rate
+
+
+def _normalize_framerate(rate, min_rate=16, max_rate=32):
+    """Limits the frame rate between min_rate and max_rate by integer multiples.
+    """
+    if rate < min_rate:
+        factor = min_rate // rate
+        if min_rate % rate > 0:
+            factor += 1
+        return rate * factor
+
+    if rate > max_rate:
+        factor = rate // max_rate
+        if rate % max_rate > 0:
+            factor += 1
+        return rate / factor
+
+    return rate
 
 
 def _monochannel_to_rgb(image, rgb):
