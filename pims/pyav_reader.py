08a9448167cf4057215e271a55d713be4e479f85
CalWelsh <cwelsh@sbacademy.org>
2021-08-28 15:37:25 -0500

    Update pyav_reader.py to make generator threadsafe
    I had an issue where code using pyav_reader.py would randomly break and throw a "ValueError: Generator Already Executing Error"; the stack indicated that the function _gen_frames was the culprit; I added a function and a decorator (line for line copied from the sources below with one change, re-naming "next" to "__next__") that makes a non-threadsafe generator threadsafe. This resolved the issue on both Windows and Mac.
    
    Sources:
    https://anandology.com/blog/using-iterators-and-generators/
    https://stackoverflow.com/questions/41194726/python-generator-thread-safety-using-keras

--- pims/pyav_reader.py
+++ pims/pyav_reader.py
@@ -27,6 +27,28 @@ def _next_video_packet(container_iter):
     raise ValueError("Could not find any video packets.")
 
 
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return self.it.next()
+          
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe.
+    """
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+    return g
+          
 class WrapPyAvFrame(object):
     def __init__(self, frame, frame_no, metadata=None):
         self.frame_no = frame_no
@@ -46,7 +68,7 @@ class WrapPyAvFrame(object):
                              frame_no=self.frame_no, metadata=self.metadata)
         return self.arr
 
-
+@threadsafe_generator
 def _gen_frames(demuxer, time_base, frame_rate=1., first_pts=0):
     for packet in demuxer:
         for frame in packet.decode():
