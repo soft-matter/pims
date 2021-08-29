2ed0ac89a2d53857925e793540cf3120cd2d3ea1
Callum Welsh <cwelsh@sbacademy.org>
2021-08-28 19:23:31 -0500

    Update pyav_reader.py

--- pims/pyav_reader.py
+++ pims/pyav_reader.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+import threading
 
 from pims.base_frames import FramesSequence
 from pims.frame import Frame
