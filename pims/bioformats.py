0f4c69e6032f2852b318a3538ae5a2c296652b37
Genevieve Buckley <30920819+GenevieveBuckley@users.noreply.github.com>
2022-04-27 17:27:12 +1000

    Set root log level with loci DebugTools
    https://docs.openmicroscopy.org/bio-formats/6.9.1/developers/logging.html
    Approach suggested by Josh Moore

--- pims/bioformats.py
+++ pims/bioformats.py
@@ -352,6 +352,7 @@ class BioformatsReader(FramesSequenceND):
             jpype.attachThreadToJVM()
 
         loci = jpype.JPackage('loci')
+        loci.common.DebugTools.enableLogging('ERROR')
 
         # Initialize reader and metadata
         self.filename = str(filename)
