f9d54d56349cfad8b52791f48dcb93a460a204ae
Eitan <Eitan.Hemed@gmail.com>
2023-05-11 17:16:04 +0300

    Added test for the handling of spaces in path when using natural_keys

--- pims/utils/tests/test_sort.py
+++ pims/utils/tests/test_sort.py
@@ -11,3 +11,25 @@ class TestNaturalSort(unittest.TestCase):
         alist = ["something1", "something12", "something17", "something2"]
         alist.sort(key=natural_keys)
         assert alist == ['something1', 'something2', 'something12', 'something17']
+
+    def test_natural_keys_with_spaces(self):
+        paths = [
+            '/data/meh/img-   19.tiff',
+            '/data/meh/img-   181.tiff',
+            '/data/meh/img-   20.tiff',
+            '/data/meh/img-    0.tiff',
+            '/data/meh/img-    1.tiff',
+            '/data/meh/img-    2.tiff',
+        ]
+
+        paths.sort(key=natural_keys)
+
+        assert paths == [
+            '/data/meh/img-    0.tiff',
+            '/data/meh/img-    1.tiff',
+            '/data/meh/img-    2.tiff',
+            '/data/meh/img-   19.tiff',
+            '/data/meh/img-   20.tiff',
+            '/data/meh/img-   181.tiff',
+
+        ]
