0cf8cd5d404d9a56f55eb3481b1a5efc2ae7c0bb
Eitan <Eitan.Hemed@gmail.com>
2023-05-11 17:09:07 +0300

    Updated existing test to use unittest

--- pims/utils/tests/test_sort.py
+++ pims/utils/tests/test_sort.py
@@ -1,7 +1,13 @@
+import unittest
+
 from pims.utils.sort import natural_keys
 
 
-def test_natural_keys():
-    alist = ["something1", "something12", "something17", "something2"]
-    alist.sort(key=natural_keys)
-    assert alist == ['something1', 'something2', 'something12', 'something17']
+class TestNaturalSort(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_natural_keys(self):
+        alist = ["something1", "something12", "something17", "something2"]
+        alist.sort(key=natural_keys)
+        assert alist == ['something1', 'something2', 'something12', 'something17']
